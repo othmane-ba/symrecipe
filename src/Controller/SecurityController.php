49f1c153b9575e08876a1716bb3fefc666571cdc
Othmane Bakkas <81847538+othmane-ba@users.noreply.github.com>
2023-03-07 15:18:51 +0100

    ✨ Adding registration's form, edit user's Form and edit user password's form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * This controller allows us to login
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     #[Route('/connexion', name: 'security.login',methods:['GET','POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -17,9 +27,43 @@ class SecurityController extends AbstractController
             'error'=>$authenticationUtils->getLastAuthenticationError()
         ]);
     }
+    /**
+     * This controller allows us to logout
+     *
+     * @return void
+     */
     #[Route('/deconneion', name: 'security.logout',methods:['GET','POST'])]
     public function logout()
     {
-        # code...
+        # Nothing to do here...
+    }
+    /**
+     *  This controller allows us to register
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/inscription','security.registration',methods:['GET','POST'])]
+    public function registration(Request $request,EntityManagerInterface $manager):Response
+    {
+        $user=new User();
+        $user->setRoles(['ROLE_USER'])->setPassword('password');
+        $form=$this->createForm(RegistrationType::class,$user);
+        $form->handleRequest($request);
+        // dd($form->getData());
+        if($form->isSubmitted() && $form->isValid()){
+            $user=$form->getData();
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre compte a bien été créé.'
+            );
+            return $this->redirectToRoute('security.login');
+        }
+        return $this->render('pages/security/registration.html.twig',[
+            'form'=>$form->createView()
+        ]);
     }
 }
---
+++ src/Controller/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserPasswordType;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * This controller allows us to edit User Profile
+     *
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit',methods:['GET','POST'])]
+    public function edit(User $user,Request $request,EntityManagerInterface $manager,UserPasswordHasherInterface $hasher): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('security.login');
+        }
+        if($this->getUser()!==$user){
+            return $this->redirectToRoute('recipe.index');
+        }
+        $form=$this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if($hasher->isPasswordValid($user,$form->getData()->getPlainPassword())){
+                $user=$form->getData();
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Les informations de votre compte ont bien été modifiées.'
+            );
+            return $this->redirectToRoute('recipe.index');
+            }else{
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrecte!'
+                );
+            }
+            
+        }
+        return $this->render('pages/user/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/utilisateur/edition-mot-de-passe/{id}','user.edit.password',methods:['GET','POST'])]
+    public function editPassword(User $user,Request $request,UserPasswordHasherInterface $hasher,EntityManagerInterface $manager):Response
+    {
+        $form=$this->createForm(UserPasswordType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if($hasher->isPasswordValid($user,$form->getData()['plainPassword'])){
+                $user->setPassword(
+                    $hasher->hashPassword(
+                        $user,
+                        $form->getData()['newPassword']
+                    ) 
+                );
+                $manager->persist($user);
+             $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifier.'
+                );
+                return $this->redirectToRoute('recipe.index');
+            }else{
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrecte!'
+                );
+            }
+        }
+        return $this->render('pages/user/edit_password.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+}
